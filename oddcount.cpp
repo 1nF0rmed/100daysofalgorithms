23f5e035eb46e5554090db15a4ebd2fae89b92a1
Pradyumna Rahul <prkinformed@gmail.com>
2018-09-26 22:56:59 +0530

    first day

---
+++ oddcount.cpp
@@ -0,0 +1,15 @@
+/*
+Problem Description:
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+oddCount(7) //=> 3, i.e [1, 3, 5]
+oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+Expect large Inputs!
+
+source: https://www.codewars.com/kata/count-odd-numbers-below-n/cpp
+*/
+
+int odd_count(int n){
+  return n/2;
+}
