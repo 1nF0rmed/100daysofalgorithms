564e4c4a87dd8d5a4cedb7a1c6276e3b4806900d
Pradyumna Rahul <prkinformed@gmail.com>
2018-11-06 17:59:15 +0530

    Update and rename algo0.cpp to areEquallyStrong.cpp

---
+++ areEquallyStrong.cpp
@@ -0,0 +1,22 @@
+/*
+Description:
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+Example
+
+For yourLeft = 10, yourRight = 15, friendsLeft = 15, and friendsRight = 10, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 10, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 9, the output should be
+areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) = false.
+
+URL:https://app.codesignal.com/arcade/intro/level-5/g6dc9KJyxmFjB98dL/description
+*/
+bool areEquallyStrong(int l, int r, int L, int R) {
+    return max(l, r)==max(L, R) && min(l, r)==min(L, R);
+}
